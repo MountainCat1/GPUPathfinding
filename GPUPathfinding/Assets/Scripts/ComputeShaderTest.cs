3652c32c1ea4e99e1effed105b75e8968e601cdc
MountainCat <tomasz.banas123@gmail.com>
2022-10-06 17:02:39 +0200

    First compute shader

--- GPUPathfinding/Assets/Scripts/ComputeShaderTest.cs
+++ GPUPathfinding/Assets/Scripts/ComputeShaderTest.cs
@@ -1,6 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ComputeShaderTest : MonoBehaviour
 {
+    [SerializeField] private ComputeShader computeShader;
+    [SerializeField] private RenderTexture renderTexture;
+
+    private void Start()
+    {
+        renderTexture = new RenderTexture(256, 256, 24)
+        {
+            enableRandomWrite = true
+        };
+        renderTexture.Create();
         
+        computeShader.SetTexture(0, "Result", renderTexture);
+        computeShader.Dispatch(0, renderTexture.width / 8, renderTexture.height / 8, 1);
+    }
 }
